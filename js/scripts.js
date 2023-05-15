4e4504d0ae550abf06ba2b791357bbd35b7da509
Svitlana-Malkova <malkova.sveta1990@gmail.com>
2023-05-15 13:05:54 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+// (function($) {
+
+// 	var tabs =  $(".tabs li a");
+  
+// 	tabs.click(function() {
+// 		var content = this.hash.replace('/','');
+// 		tabs.removeClass("active");
+// 		$(this).addClass("active");
+//     $("#content").find('p').hide();
+//     $(content).fadeIn(200);
+// 	});
+
+// })(jQuery);
+
+$(document).ready(function () {
+  var tabs = $(".content"); // тут должны быть табы а не кнопки, сами табы у нас имеют класс content, его достаточно
+  $(tabs[0]).show();
+  $(".experience-name").click(function (e) {
+    e.preventDefault(); // чтобы после нажатия на кнопку, экран не прыгал наверх
+    // var content = this.hash.replace("/", ""); // не понял, зачем эта штука 
+    
+    tabs.removeClass("active"); // 
+    $(this).addClass("active"); // в обоих этих строчках удаляем и добавляем класс, но он ни на
+    // что не влияет, по скольку мы используем только JS без CSS.
+
+    // Скрыть все элементы <li> с классом "content"
+    $("li p.content").hide();
+    // Найти соответствующий элемент <li> с помощью атрибута "data-target"
+    var target = $(this).attr("data-target");
+    $(target).fadeIn(200);
+    // Отобразить найденный элемент <li> с классом "content"
+    $(target).show();
+  });
+});
+
+
+/*$(document).ready(function () {
+  $(".carousel").slick({
+      dots: true,
+      arrows: false,
+});
+});*/
